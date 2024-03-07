09d166ea30f23a2672845d50a7ac98eac4c3c032
Nicolas Baeza <ianbaezarivera@gmail.com>
2024-03-07 19:04:20 -0300

    First Commit

---
+++ testNETBackend/Program.cs
@@ -0,0 +1,82 @@
+﻿
+
+
+//var sale = new SaleWithTax(15, 1.16m);
+//sale.Total = 15;
+
+//var message = sale.GetInfo();
+
+//Console.WriteLine(message);
+
+//class SaleWithTax : Sale
+//{
+//    public decimal Tax {  get; set; }   
+//    public SaleWithTax(decimal total, decimal tax) : base(total)
+//    {
+//        this.Tax = tax;
+//    }
+
+//    public override string GetInfo()
+//    {
+//        return "El total es de " + Total + " más impuestos: " + Tax;
+//    }
+
+//    public string GetInfo(string message)
+//    {
+//        return message;
+//    }
+
+//    public string GetInf(int a)
+//    {
+//        return a.ToString();
+//    }
+//}
+
+//class Sale
+//{
+//    public decimal Total { get; set;}
+
+//    public Sale(decimal total) 
+//    {
+//        this.Total = total;
+//    }
+
+//    public virtual string GetInfo()
+//    {
+//        return "El total es " + Total;
+//    }
+
+//}
+
+///////////////////////////
+
+
+
+interface ISale
+{
+    decimal Total { get; set; }  
+}
+
+interface ISave
+{
+    public void Save();
+}
+
+public class Sale : ISale, ISave
+{
+    public decimal Total { get; set; } 
+    public void Save()
+    {
+        Console.WriteLine("Se guardo en BD");
+    }
+}
+
+public class Beer : ISave
+{
+    public void Save()
+    {
+        Console.WriteLine("Se guardo en Servicio");    
+    }
+}
+
+
